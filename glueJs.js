14d704d71c7c0eae8dffa222b1241b1a1bf1fdd6
Alexandre PIEL <alexandre.piel@gmail.com>
2015-09-24 15:58:27 +0200

    use cacheData & update dom only if necessary

--- glueJs.js
+++ glueJs.js
@@ -7,6 +7,7 @@
     };
     glueJs.prototype = {
         data: {},
+        cacheData: {},
         init: function () { 
             var me = this;
             me.data = {};
@@ -15,16 +16,16 @@
         _findBase: function () {
             var me = this;
             $('*[gl-base]').each(function() {
-                me._findPath($(this), $(this).attr('gl-base').split('.'));
+                me._findPath($(this), $(this).attr('gl-base'));
             });
         },
         _findPath: function(dom, path) {
             var me = this;
             var hasChild = 0;
-            dom.find('*[gl-path]').each(function() {
+            dom.find('*[gl-path]').each(function() { // foreach of them set gl-id
                 var parent = $(this).parent().closest('*[gl-base],*[gl-path]'); 
                 if (parent[0] == dom[0]) {
-                    me._findPath($(this), path.concat($(this).attr('gl-path').split('.')));
+                    me._findPath($(this), path+'.'+$(this).attr('gl-path'));
                     hasChild++;
                 }
             });
@@ -35,18 +36,16 @@
         _initValue: function(dom, path) {
             var me = this;
             var val = dom.is(':input') ? dom.val() : dom.text();
-            dom.attr('gl-id', path.join('.'));
-            me._setData(path, val);
+            dom.attr('gl-id', path);
+            me._setCacheData(path, val);
         },
-        _setData: function(path, val) {
-            var me = this;
-            var name = path.pop();
-            var data = me.data;
-            $.each(path, function() {
-                if (typeof(data[this]) === 'undefined') data[this] = {};
-                data = data[this];
-            });
-            data[name] = val;            
+        _getCacheData: function(path) {
+          var me = this;
+          return typeof(me.cacheData[path]) !== 'undefined' ? me.cacheData[path] : null;
+        },
+        _setCacheData: function(path, val) {
+          var me = this;
+          me.cacheData[path] = val;
         },
         getPath: function(dom) {
             var path = [];
@@ -55,32 +54,49 @@
                 dom = dom.closest('*[gl-base],*[gl-path]');
                 isBase = dom.is('*[gl-base]');
                 var attr = isBase ? dom.attr('gl-base') : dom.attr('gl-path');
-                path = attr.split('.').concat(path);
+                path.push(attr);
                 dom = dom.parent();
             }
-            return path;
+            return path.join('.');
+        },
+        _setData: function(data, path, val) {
+            var me = this;
+            path = path.split('.');
+            var name = path.pop();
+            $.each(path, function() {
+                if (typeof(data[this]) === 'undefined') data[this] = {};
+                data = data[this];
+            });
+            data[name] = val;     
         },
         get: function(path) {
             var me = this;
             if (!Array.isArray(path)) {
-                if (typeof(path) === 'object') path = me.getPath(path);
-                else path = path.split('.');
+              if (typeof(path) === 'undefined') path = '';
+              else if (typeof(path) === 'object') path = me.getPath(path);
             } 
-            var data = me.data;
-            $.each(path, function() {
-                if (typeof(data[this]) === 'undefined') { data = null; return false; };
-                data = data[this];
+            var data = {};
+            $.each(me.cacheData, function(key, val) {
+              if (key.indexOf(path) === 0) {
+                var subPath = path.length ? key.substr(path.length+1) : key;
+                if (subPath === '') {
+                  data = val;
+                } else {
+                  me._setData(data, subPath, val);
+                }
+              }
             });
             return data;
         },
         set: function(path, val) {
             var me = this;
             if (typeof(path) !== 'string') path = path.attr('gl-id');
-            if (typeof(val) === 'undefined') val = dom.is(':input') ? dom.val() : dom.text();
-            $('*[gl-id="'+path+'"]').each(function() {
-                me._val($(this), val);
-            });
-            me._setData(path.split('.'), val);
+            if (me._getCacheData(path) !== val) {
+              $('*[gl-id="'+path+'"]').each(function() {
+                  me._val($(this), val);
+              });
+              me._setCacheData(path, val);
+            }
             return me;
         },
         _pull: function(dom, variables) {
@@ -93,7 +109,7 @@
 					});
 				}
                 dom.attr('gl-id', path);
-                me._val(dom, me.get(path.split('.')));
+                me._val(dom, me._getCacheData(path));
             } else if (typeof(variables)!== 'undefined' && dom.is('[gl-var]')) {
                 var attr = dom.attr('gl-var');
                 if (typeof(variables[attr]) !== 'undefined') {
@@ -114,8 +130,14 @@
             me.set(dom, dom.is(':input') ? dom.val() : dom.text());
         },
         _val: function(dom, val) {
-            if (dom.is(':input')) dom.val(val);
-            else dom.text(val);  
+            if (dom.is(':input')) {
+              if (dom.val() !== val)
+                dom.val(val);
+            }
+            else {
+              if (dom.text() !== val)
+                dom.text(val);
+            }  
         },
     };
     
